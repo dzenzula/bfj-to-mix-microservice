c56a6fbe67eb862aca6a3c3c7176d0adb4c1e47a
Artem Dzenzura <98640943+dzenzula@users.noreply.github.com>
2023-05-12 08:43:02 +0300

    Add files via upload

---
+++ models/chemicalDTO.go
@@ -0,0 +1,14 @@
+package models
+
+type ChemicalDTO struct {
+	NMix       int     `json:"nMix"`
+	Ladle      string  `json:"ladle"`
+	Proba      int     `json:"proba"`
+	NumTaphole int     `json:"numTaphole"`
+	DT         *string `json:"dt"`
+	Si         float64 `json:"si"`
+	Mn         float64 `json:"mn"`
+	S          float64 `json:"s"`
+	P          float64 `json:"p"`
+	Belong     string  `json:"belong"`
+}
